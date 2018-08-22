3028ad7d7d28b8e996150ea1713b7b982af81534
Guilherme Sernajoto <guilhermesernajoto@hotmail.com>
2018-08-22 14:43:46 -0300

    Add graphql server

--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -4,13 +4,32 @@ import Post from '../Post/index.js'
 
 
 class Posts extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      pics: []
+    };
+  };
+
+  componentDidMount() {
+    // For local server use: 'http://localhost:4000/'
+    fetch('https://cloud-graphql-server.herokuapp.com/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query: '{ cloud { name link } }' }),
+    })
+      .then(res => res.json())
+      .then(res => {
+        let pic = res.data.cloud.map(p => p);
+        this.setState({ pics: pic });
+      });
+  }
+
   render() {
-    // Testing
-    let pics = [{ link: 'https://goo.gl/6KKfm5', name: 'My dog top' }, { link: 'https://goo.gl/6KKfm5', name: 'Dog topperson' }];
-    
     return (
       <ScrollView>
-        {pics.map( (p, k) => <Post key={k} image={p.link} caption={p.name} />)}
+        {this.state.pics.map((p, k) => <Post key={k} image={p.link} caption={p.name} />)}
       </ScrollView>
     );
   }
